3e9a8619212f12e0d48da9bf0002ae2972c2ef94
pawnikhanna <51890749+pawnikhanna@users.noreply.github.com>
2020-08-19 14:49:23 +0530

    Update students.js

--- routes/students.js
+++ routes/students.js
@@ -1,34 +1,33 @@
-const studentRoutes = (app, fs) => {
+const express = require('express');
+const fs = require('fs');
+const app = express.Router();
+const studentPath = './data/students.json';
 
-    const studentPath = './data/students.json';
+app.get("/", (req, res) => {
+    let students = JSON.parse(fs.readFileSync(studentPath));
+    res.send({ students });
+});
 
-    app.get("/students", (req, res) => {
-        let students = JSON.parse(fs.readFileSync(studentPath));
-        res.send({ students });
-      });
-
-    app.get("/students/:id", (req, res) => {
-        let students = JSON.parse(fs.readFileSync(studentPath));
-        let student = students.find((student) => {
-          return student.id == req.params.id;
-        });
-        if (!student) return res.status(400).json({error: `No student found with ID ${req.params.id}`});
-        res.send({ student });
+app.get("/:id", (req, res) => {
+    let students = JSON.parse(fs.readFileSync(studentPath));
+    let student = students.find((student) => {
+      return student.id == req.params.id;
     });
+    if (!student) return res.status(400).json({error: `No student found with ID ${req.params.id}`});
+    res.send({ student });
+});
 
-    app.post("/students", (req, res) => {
-        let name = req.body.name;
-        let description = req.body.description;
-        if (!name || !description) {
-          return res.status(400).json({ error: "Unidentified" });
-        }
-        let data = JSON.parse(fs.readFileSync(studentPath));
-        data.push({ id: data.length + 1, name, description });
-        jsonData = JSON.stringify(data, null, 2);
-        fs.writeFile(studentPath, jsonData, "utf8", () => {
-        res.json({ success: true });
-        });
+app.post("/", (req, res) => {
+    let name = req.body.name;
+    let description = req.body.description;
+    if (!name || !description) {
+      return res.status(400).json({ error: "Unidentified" });
+    }
+    let data = JSON.parse(fs.readFileSync(studentPath));
+    data.push({ id: data.length + 1, name, description });
+    jsonData = JSON.stringify(data, null, 2);
+    fs.writeFile(studentPath, jsonData, "utf8", () => {
+    res.json({ success: true });
     });
-};
-
-module.exports = studentRoutes;
+});
+module.exports = app;
